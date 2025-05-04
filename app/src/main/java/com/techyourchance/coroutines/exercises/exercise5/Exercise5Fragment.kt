694c88ca10ae907ed0728fc3f728db8fedc3f936
Jakhongir Mannonov <jakhongir.m@payme.uz>
2025-05-04 13:49:53 +0500

    exercise 5 done

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise5/Exercise5Fragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise5/Exercise5Fragment.kt
@@ -12,8 +12,8 @@ import androidx.fragment.app.Fragment
 import com.techyourchance.coroutines.R
 import com.techyourchance.coroutines.common.BaseFragment
 import com.techyourchance.coroutines.common.ThreadInfoLogger
-import com.techyourchance.coroutines.exercises.exercise1.GetReputationEndpoint
 import com.techyourchance.coroutines.home.ScreenReachableFromHome
+import com.techyourchance.coroutines.solutions.exercise5.GetReputationUseCase
 import kotlinx.coroutines.*
 
 class Exercise5Fragment : BaseFragment() {
@@ -26,14 +26,12 @@ class Exercise5Fragment : BaseFragment() {
     private lateinit var btnGetReputation: Button
     private lateinit var txtElapsedTime: TextView
 
-
-    private lateinit var getReputationEndpoint: GetReputationEndpoint
+    private lateinit var getReputationUseCase: GetReputationUseCase
 
     private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getReputationEndpoint = compositionRoot.getReputationEndpoint
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -57,7 +55,7 @@ class Exercise5Fragment : BaseFragment() {
             logThreadInfo("button callback")
             job = coroutineScope.launch {
                 btnGetReputation.isEnabled = false
-                val reputation = getReputationForUser(edtUserId.text.toString())
+                val reputation = getReputationUseCase.getReputationForUser(edtUserId.text.toString())
                 Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT).show()
                 btnGetReputation.isEnabled = true
             }
@@ -70,13 +68,6 @@ class Exercise5Fragment : BaseFragment() {
         super.onStop()
         job?.cancel()
         btnGetReputation.isEnabled = true
-    }
-
-    private suspend fun getReputationForUser(userId: String): Int {
-        return withContext(Dispatchers.Default) {
-            logThreadInfo("getReputationForUser()")
-            getReputationEndpoint.getReputation(userId)
-        }
     }
 
     private fun logThreadInfo(message: String) {
